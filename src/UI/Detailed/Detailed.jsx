97f5d23cbcf8340ab68808fd6116ec439d259f8c
Ego1stCompany <151455987+Ego1stCompany@users.noreply.github.com>
2024-03-20 01:04:17 +0300

    part 1

---
+++ src/UI/Detailed/Detailed.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Detailed.module.css'
+
+const Detailed = () => {
+    return (
+        <button className={styles.header__content_button}>
+            подробнее
+        </button>
+    );
+};
+
+export default Detailed;
